8d4528dd54182634df0a75e8bc3ded3095afadfb
marcus <mpet0303@uni.sydney.edu.au>
2024-12-10 18:04:17 +0800

    Updated prisma import to import generated client instead of package

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '.prisma/client'
 
 
 let prisma
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+    prisma = new PrismaClient()
 } else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient()
-  }
-  prisma = (global as any).prisma
+    if (!(global as any).prisma) {
+        (global as any).prisma = new PrismaClient()
+    }
+    prisma = (global as any).prisma
 }
 
 export default prisma as PrismaClient
