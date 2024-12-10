d667ff7dd956d95d604944428746ac65a0ac6dca
marcus <mpet0303@uni.sydney.edu.au>
2024-12-10 21:09:30 +0800

    configured CORS

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,28 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     output: 'standalone',
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: '*',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
